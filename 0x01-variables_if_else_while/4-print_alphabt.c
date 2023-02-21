531d8c86bf92931a9793e643220fea0378eb925d
invalid8 <b.fadamitan2019@gmail.com>
2023-02-20 16:51:40 -0800

    my commit at Mon 20 Feb 2023 04:51:40 PM PST

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char low, high;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low == 'e' || low == 'q')
+		{
+			continue;
+		}
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
