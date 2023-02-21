50d983a10353fa30090a26af2fc59908aad72277
invalid8 <b.fadamitan2019@gmail.com>
2023-02-21 09:17:38 -0800

    my commit at Tue 21 Feb 2023 09:17:38 AM PST

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,15 +8,6 @@
 * Return: Always 0 (Success)
 */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
 /* this function is to print alphabet */
 void print_alphabet(void)
 {
