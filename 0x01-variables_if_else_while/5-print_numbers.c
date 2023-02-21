cced9414e5f2daf34cd4bb4566405c2b21445e2f
invalid8 <b.fadamitan2019@gmail.com>
2023-02-20 16:57:30 -0800

    my commit at Mon 20 Feb 2023 04:57:30 PM PST

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		printf("%d", count);
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
