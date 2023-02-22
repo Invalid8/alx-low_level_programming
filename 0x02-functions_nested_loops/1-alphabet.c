db6e6202974e249dd433c9fc4798de93df64f38c
invalid8 <b.fadamitan2019@gmail.com>
2023-02-22 09:01:52 -0800

    my commit at Wed 22 Feb 2023 09:01:52 AM PST

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-/* this function is to print alphabet */
+
 void print_alphabet(void);
+
+/* This function is for printing alphabets out */
 
 void print_alphabet(void)
 {
-  int i;
-  
+	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
-	{
 		putchar(i);
-	}
 
 	putchar('\n');
 }
