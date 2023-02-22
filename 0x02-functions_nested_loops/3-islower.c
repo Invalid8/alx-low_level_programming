fe3f3a46032c92bd02f6fcdec8b1e135861bf273
invalid8 <b.fadamitan2019@gmail.com>
2023-02-22 09:31:19 -0800

    my commit at Wed 22 Feb 2023 09:31:19 AM PST

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int _islower(int c);
+
+/**
+* _islower - Entry point
+* c - integer
+* Description: 'Checking numbers out'
+* Return: 1 or 0
+*/
+
+int _islower(int c)
+{
+	if (c._islower())
+		return (1);
+	else
+		return (0);
+}
