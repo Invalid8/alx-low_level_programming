1e32badc8ecee3557e55844164c1dc0613e990f2
invalid8 <b.fadamitan2019@gmail.com>
2023-02-22 17:18:49 -0800

    my commit at Wed 22 Feb 2023 05:18:49 PM PST

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add(int a, int b);
+
+/**
+* add - Entry point
+* @a: input
+* @b: the input
+* Description: 'This function is for adding'
+* Return: sum of two
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
