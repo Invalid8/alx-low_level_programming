97fa87e8604e4a00b0e07d5931f9977088e765f3
invalid8 <b.fadamitan2019@gmail.com>
2023-02-22 17:26:08 -0800

    my commit at Wed 22 Feb 2023 05:26:08 PM PST

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void print_times_table(int n);
+
+/**
+* print_times_table - Entry point
+* @n: the input
+* Description: 'This function is for checking out timestable'
+* Return: nothing
+*/
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			putchar('0');
+			for (j = 1; j <= n; j++)
+				putchar(i * j);
+			putchar('\n');
+		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(' ');
+		putchar(' ');
+		putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(' ');
+		putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(n / 100 + '0');
+		putchar(n / 10 % 10 + '0');
+		putchar(n % 10 + '0');
+	}
+}
