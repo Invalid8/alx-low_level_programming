f56956076343fa1b32b37692ee2d51c7ca93757d
invalid8 <b.fadamitan2019@gmail.com>
2023-02-22 17:09:23 -0800

    my commit at Wed 22 Feb 2023 05:09:23 PM PST

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void jack_bauer(void);
+
+/**
+* jack_bauer - Entry point
+* @c: the input
+* Description: 'This function is for get time from
+* 0:00 to 23: 59'
+* Return: All time between
+*/
+
+int main()
+{
+	jack_bauer();
+	return 0;
+}
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			putchar((H / 10) + '0');
+			putchar((H % 10) + '0');
+			putchar(':');
+			putchar((M / 10) + '0');
+			putchar((M % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
