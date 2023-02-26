8deefe09ee57a6ff6d88f6217c224a3ff32e622e
invalid8 <b.fadamitan2019@gmail.com>
2023-02-26 15:05:26 -0800

    my new commit at Sun 26 Feb 2023 03:05:26 PM PST

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,30 +11,23 @@ void print_triangle(int size);
 
 void print_triangle(int size)
 {
-	char s = '#';
+	int i = 0, j, n = size - 1;
 
-	int n, sn1;
-
-	n = 0;
-	sn1 = size - 1;
-
-	while (size > n)
+	if (size > 0)
 	{
-		int i;
-
-		for (i = 0; i < sn1; i++)
+		for (; i < size; i++)
 		{
-			putchar(' ');
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					putchar(' ');
+				else
+					putchar('#');
+			}
+			n--;
+			putchar('\n');
 		}
-
-		for (i = size - sn1; i > 0; i--)
-		{
-			putchar(s);
-		}
-
-		putchar('\n');
-
-		sn1--;
-		n++;
 	}
+	else
+		putchar('\n');
 }
