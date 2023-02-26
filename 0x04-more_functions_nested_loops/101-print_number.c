15463747c1a6064b59d930c1bcac693402cb2113
invalid8 <b.fadamitan2019@gmail.com>
2023-02-26 15:43:24 -0800

    my new commit at Sun 26 Feb 2023 03:43:24 PM PST

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 void print_integer(int m);
 
 /**
@@ -7,15 +9,15 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-    if (n == 0)
-        putchar('0');
-    else if (n < 0)
-    {
-        putchar('-');
-        print_integer(n * -1);
-    }
-    else
-        print_integer(n);
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
+	else
+		print_integer(n);
 }
 
 /**
@@ -25,11 +27,11 @@ void print_number(int n)
  */
 void print_integer(int m)
 {
-    int i = 1000000000;
+	int i = 1000000000;
 
-    for (; i >= 1; i /= 10)
-        if (m / i != 0)
-        {
-            putchar((m / i) % 10 + '0');
-        }
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
 }
