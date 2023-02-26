53b239ca68235594f3c3b72b7ad0d9a6ef308a92
invalid8 <b.fadamitan2019@gmail.com>
2023-02-26 14:06:06 -0800

    my new commit at Sun 26 Feb 2023 02:06:06 PM PST

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -11,17 +11,16 @@ int _isdigit(int c);
 
 int _isdigit(int c)
 {
-    char i;
-    int r;
+	char i;
+	int r;
 
-    r = 0;
+	r = 0;
 
-    for (i = '0'; i <= '9'; i++)
-        if (c == i)
-        {
-            r = 1;
-            break;
-        }
-
-    return (r);
+	for (i = '0'; i <= '9'; i++)
+		if (c == i)
+		{
+			r = 1;
+			break;
+		}
+	return (r);
 }
