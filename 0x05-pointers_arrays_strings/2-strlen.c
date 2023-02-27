83c8eee45b155201d8711b72a64ae489c54c9385
invalid8 <b.fadamitan2019@gmail.com>
2023-02-27 14:13:24 -0800

    my new commit at Mon 27 Feb 2023 02:13:24 PM PST

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strlen - this is a function for check out
+ * @s: this is the input to work on
+ * Decription: 'This function is to check string lenght'
+ * Return: 0
+ */
+
+int _strlen(char *s);
+
+int _strlen(char *s)
+{
+    int n;
+
+    while (s[n] != '\0')
+        n++;
+
+    return (n);
+}
