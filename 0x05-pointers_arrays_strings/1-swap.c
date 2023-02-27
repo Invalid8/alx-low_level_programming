340d31bf77a1486e678a73aadb31193b2b9f9f7c
invalid8 <b.fadamitan2019@gmail.com>
2023-02-27 11:59:43 -0800

    my new commit at Mon 27 Feb 2023 11:59:43 AM PST

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * swap_int - this is a function for check out
+ * @a: this is the input to work on
+ * @b: this is also an int input
+ * Decription: 'This function is to swap locations'
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b);
+
+void swap_int(int *a, int *b)
+{
+	int *x = NULL;
+
+	*x = *b;
+	*b = *a;
+	*a = *x;
+}
