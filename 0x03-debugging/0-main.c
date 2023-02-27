c4a8fbb5d7fbe56cd06bdeaf5b93b193cb7bd429
invalid8 <b.fadamitan2019@gmail.com>
2023-02-26 16:16:57 -0800

    my new commit at Sun 26 Feb 2023 04:16:57 PM PST

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for Upper.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-  char c;
+	char c;
 
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-  return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
