e62923733c625d39ab1c0dc8fa9b7ddd25780c1b
invalid8 <b.fadamitan2019@gmail.com>
2023-02-28 04:21:51 -0800

    my new commit at Tue 28 Feb 2023 04:21:51 AM PST

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - this is a function for check out
+ * @a: this is the array
+ * @n: the lenght of array
+ * Decription: 'This function is to print array elements
+ * Return: 0
+ */
+
+void print_array(int *a, int n);
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+}
