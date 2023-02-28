200cded9a4cd3ffbafe19e43930220965beb4ddc
invalid8 <b.fadamitan2019@gmail.com>
2023-02-28 06:30:38 -0800

    my new commit at Tue 28 Feb 2023 06:30:38 AM PST

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - this is a function for check out
+ * @s: this is the array something
+ * Decription: convert srting to integer
+ * Return: 0
+ */
+
+int _atoi(char *s);
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
